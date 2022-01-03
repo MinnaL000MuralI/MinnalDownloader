581f448c6e857ceb02d440712cd76f0ff3e57514
MinnaL000MuralI <97011856+MinnaL000MuralI@users.noreply.github.com>
2022-01-03 14:06:34 +0530

    Update consts.py

--- consts.py
+++ consts.py
@@ -3,20 +3,23 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def welcome_msg():
-    welcome_msg = '''Hello my*@👋🏻😇\n\n oru Instagram username thannal aynte DP angot theerum'''
+    welcome_msg = '''<b>Welcome To the Bot</b>🖐🖐
+ <i>Send me anyones instagram username or profile url to get their DP</i>
+ ex : <b>virat.kohli</b> , <b>thenameisyash</b> etc'''
 
     return welcome_msg
 
 
 def acc_type(val):
     if(val):
-        return "🔒Pootti vechekkuva🔒"
+        return "🔒Private🔒"
     else:
-        return "🔓Thuranna manassaa🔓"
+        return "🔓Public🔓"
 
 
 def create_caption(user):
-    caption_msg = f'''📛*Aynte Peru*📛: {es(user.full_name,version=2)} \n😁*Followers*😁: {es(str(user.followers),version=2)} \n🤩*Following*🤩: {es(str(user.followees),version=2)}\\n🧐*Account Type*🧐: {acc_type(user.is_private)} \n\nNandhi veendum Varika (Thanthakk vilikkaruth)\n\nEee bot njangalude swakarya Ahankaramaanu by @darkprince163 & @the_hsk'''
+    caption_msg = f'''📛*Name*📛: {es(user.full_name,version=2)} \n😁*Followers*😁: {es(str(user.followers),version=2)} \n🤩*Following*🤩: {es(str(user.followees),version=2)}\
+        \n🧐*Account Type*🧐: {acc_type(user.is_private)} \n\nThank You For Using The bot 😀😀'''
 
     return caption_msg
 
@@ -36,4 +39,4 @@ def get_username(url):
 
 
 ratingkey = [[InlineKeyboardButton(
-    "⚠️ivide click cheyyaruth⚠️", url="https://t.me/SJ_HSK_bots/2")]]
+    "Rate me ❤️", url="https://t.me/BotsArchive/1657")]]
